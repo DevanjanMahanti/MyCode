c9c5cdbd8f2a61f580f68a98f4b8c249c870edca
[DevanjanMahanti] <[devanjanmahanti4@gmail.com]>
2019-04-17 22:09:19 +0530

    [New File]

---
+++ Prime_no_using_Loops.py
@@ -0,0 +1,20 @@
+print ("This for Prime no printing prog with loop")
+
+for i in range(1,30):
+    counter = 0
+    j = 2
+
+    while j<i :
+        if i%j==0 :
+            counter = 1
+            j=j+1
+
+        else :
+
+            j=j+1
+            
+    if counter== 0:
+        print (str(i) + "This is a prime number")
+
+    else :
+        counter = 0
